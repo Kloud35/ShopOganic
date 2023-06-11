2f1a7588e09e8af8758e6833d3c6bebdca70c3c7
admin <admin@DESKTOP-ABEN704>
2023-06-12 01:57:31 +0700

    fix program

--- ShopOganic/Program.cs
+++ ShopOganic/Program.cs
@@ -1,7 +1,50 @@
+﻿using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using AspNetCoreHero.ToastNotification.Extensions;
+using AspNetCoreHero.ToastNotification.Notyf;
+using ShopOganicAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<INotyfService, NotyfService>();
+builder.Services.AddNotyf(config =>
+{
+    config.DurationInSeconds = 3;
+    config.IsDismissable = true;
+    config.Position = NotyfPosition.TopRight;
+});
+
+builder.Services.AddMvc().AddSessionStateTempDataProvider();
+builder.Services.AddAuthentication("MyCookie").AddCookie("MyCookie", config =>
+{
+    config.Cookie.Name = "MyCookie";
+    config.LoginPath = "/Admin/Home/Login";
+    config.ReturnUrlParameter = "itworkingggggg";
+}).AddCookie("sthelse", config =>
+{
+    config.Cookie.Name = "sthelse";
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireRole("Admin"); // Yêu cầu người dùng có vai trò "Admin"
+    });
+    options.AddPolicy("EmployeePolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireRole("Employee");
+    });
+    options.AddPolicy("CustomerPolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireRole("Customer");
+    });
+});
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -17,14 +60,18 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseNotyf();
+app.UseSession();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-      name: "Admin",
-      pattern: "{area:exists}/{controller=LoginAdminController}/{action=Index}/{id?}"
-    );
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Login}/{id?}"
+              );
 });
 app.MapControllerRoute(
     name: "default",
